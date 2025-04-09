297793ef64fbbb5792224c6480333f9e8b3bdc0c
chwerob <vminolas@gmail.com>
2025-04-09 08:08:58 +0200

    feat: add a 3x3 GameBoard

---
+++ script.js
@@ -0,0 +1,58 @@
+const rows = 3;
+const cols = 3;
+const matrix = [];
+
+for (let i = 0; i < rows; i++) {
+  const row = [];
+  for (let j = 0; j < cols; j++) {
+    row.push(0);
+  }
+  matrix.push(row);
+}
+
+//console.log(matrix);
+
+function Gameboard() {
+  const rows = 3;
+  const cols = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  console.log(board);
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+
+  return {
+    getBoard,
+    printBoard
+  };
+}
+
+function Cell() {
+    let value = 0;
+  
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
+  }
+  
